98266052288fd9a07f35690a4e200e1702f7df0a
jfdelnero <jfdelnero@63fa2f70-6a9b-4bc4-b855-9c6a949b1d69>
2011-12-11 13:56:27 +0000

    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code@284 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

---
+++ HxCFloppyEmulator/vfd_hxc/tags/revision1/src/libhxcfe/libhxcfe.h
@@ -0,0 +1,156 @@
+/*
+//
+// Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011 Jean-François DEL NERO
+//
+// This file is part of HxCFloppyEmulator.
+//
+// HxCFloppyEmulator may be used and distributed without restriction provided
+// that this copyright statement is not removed from the file and that any
+// derivative work contains the original copyright notice and the associated
+// disclaimer.
+//
+// HxCFloppyEmulator is free software; you can redistribute it
+// and/or modify  it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// HxCFloppyEmulator is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//   See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with HxCFloppyEmulator; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+*/
+
+//////////////////////////////////////////////////////////
+// Output Message  level
+#define MSG_INFO_0 0
+#define MSG_INFO_1 1
+#define MSG_WARNING 2
+#define MSG_ERROR 3
+#define MSG_DEBUG 4
+// Output functions 
+typedef int (*HXCPRINTF_FUNCTION)(int MSGTYPE,char * string, ...);
+typedef int (*DISPLAYTRACKPOS_FUNCTION)(unsigned int current,unsigned int total);
+//////////////////////////////////////////////////////////
+
+#define HXCFE_VALIDFILE			1
+#define HXCFE_NOERROR			0 
+#define HXCFE_ACCESSERROR		-1
+#define HXCFE_BADFILE			-2
+#define HXCFE_FILECORRUPTED		-3 
+#define HXCFE_BADPARAMETER		-4
+#define HXCFE_INTERNALERROR		-5
+#define HXCFE_UNSUPPORTEDFILE	-6
+
+typedef void HXCFLOPPYEMULATOR;
+typedef void FLOPPY;
+typedef void FBuilder; 
+typedef void SECTORSEARCH;
+typedef void SECTORCONFIG;
+////////////////////////////////////////////////////////////////////////////////////////////
+// Init Function
+
+HXCFLOPPYEMULATOR* hxcfe_init(void);
+int hxcfe_getversion(HXCFLOPPYEMULATOR* floppycontext,char * version,unsigned int * size1,char *copyright,unsigned int * size2);
+int hxcfe_set_outputfunc(HXCFLOPPYEMULATOR* floppycontext,HXCPRINTF_FUNCTION hxc_printf);
+void hxcfe_deinit(HXCFLOPPYEMULATOR* hxcfe);
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// File image functions
+
+int hxcfe_getcontainerid(HXCFLOPPYEMULATOR* floppycontext,int index,char * id,char * desc);
+int hxcfe_select_container(HXCFLOPPYEMULATOR* floppycontext,char * container);
+
+FLOPPY * hxcfe_floppy_load(HXCFLOPPYEMULATOR* floppycontext,char* imgname,int * err_ret);
+int hxcfe_floppy_unload(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * floppydisk);
+int hxcfe_floppy_export(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * newfloppy,char* imgname);
+
+int hxcfe_getfloppysize(HXCFLOPPYEMULATOR* floppycontext,FLOPPY *fp,int * nbsector);
+
+int hxcfe_getNumberOfTrack(HXCFLOPPYEMULATOR* floppycontext,FLOPPY *fp);
+int hxcfe_getNumberOfSide(HXCFLOPPYEMULATOR* floppycontext,FLOPPY *fp);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// Custom Image/floppy generation functions
+
+FBuilder* hxcfe_init_floppy(HXCFLOPPYEMULATOR* floppycontext,int nb_of_track,int nb_of_side);
+
+int	hxcfe_pushTrack (FBuilder*,unsigned int rpm,int number,int side,int type);
+int hxcfe_setTrackInterleave(FBuilder*,int interleave);
+int hxcfe_setTrackSkew(FBuilder*,int skew);
+
+int hxcfe_setIndexPosition(FBuilder*,int position,int allowsector);
+int hxcfe_setIndexLength(FBuilder*,int Length);
+
+int hxcfe_setTrackBitrate(FBuilder*,int bitrate);
+
+int hxcfe_addSector(FBuilder* fb,int sectornumber,int side,int track,unsigned char * buffer,int size);
+
+int hxcfe_setSectorBitrate(FBuilder* fb,int bitrate);
+
+int hxcfe_setSectorGap3(FBuilder* fb,unsigned char Gap3);
+int hxcfe_setSectorSizeID(FBuilder* fb,unsigned char sectorsizeid);
+int hxcfe_setSectorFill(FBuilder*,unsigned char fill);
+
+int hxcfe_setSectorEncoding(FBuilder*,int encoding);
+
+int hxcfe_setSectorDataCRC(FBuilder*,unsigned short crc);
+int hxcfe_setSectorHeaderCRC(FBuilder*,unsigned short crc);
+
+int hxcfe_setSectorDataMark(FBuilder*,unsigned char datamark);
+
+int hxcfe_popTrack (FBuilder* fb);
+
+FLOPPY* hxcfe_get_floppy(FBuilder* fb);
+
+
+SECTORSEARCH* hxcfe_init_sectorsearch(HXCFLOPPYEMULATOR* floppycontext,FLOPPY *fp);
+SECTORCONFIG* hxcfe_getnextsector(SECTORSEARCH* ss,int track,int side);
+SECTORCONFIG* hxcfe_searchsector(SECTORSEARCH* ss,int track,int side,int id);
+int hxcfe_getsectorsize(SECTORSEARCH* ss,SECTORCONFIG* sc);
+unsigned char * hxcfe_getsectordata(SECTORSEARCH* ss,SECTORCONFIG* sc);
+int hxcfe_readsectordata(SECTORSEARCH* ss,int track,int side,int sector,int numberofsector,int sectorsize,unsigned char * buffer);
+void hxcfe_free_sectorconfig(SECTORSEARCH* ss,SECTORCONFIG* sc);
+void hxcfe_deinit_sectorsearch(SECTORSEARCH* ss);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// Floppy functions
+
+#define IBMPC_DD_FLOPPYMODE				0x00
+#define IBMPC_HD_FLOPPYMODE				0x01
+#define ATARIST_DD_FLOPPYMODE			0x02
+#define ATARIST_HD_FLOPPYMODE			0x03
+#define AMIGA_DD_FLOPPYMODE				0x04
+#define AMIGA_HD_FLOPPYMODE				0x05
+#define CPC_DD_FLOPPYMODE				0x06
+#define GENERIC_SHUGART_DD_FLOPPYMODE	0x07
+#define IBMPC_ED_FLOPPYMODE				0x08
+#define MSX2_DD_FLOPPYMODE				0x09
+#define C64_DD_FLOPPYMODE				0x0A
+#define EMU_SHUGART_FLOPPYMODE			0x0B
+
+enum {
+	DOUBLESTEP = 1,
+	INTERFACEMODE = 2
+};
+
+enum {
+	SET = 0,
+	GET = 1
+};
+
+int hxcfe_floppy_getset_params(HXCFLOPPYEMULATOR* floppycontext,FLOPPY * newfloppy,unsigned char dir,unsigned short param,void * value);
+
+#define IBMFORMAT_SD 0x1
+#define IBMFORMAT_DD 0x2
+#define ISOFORMAT_SD 0x3
+#define ISOFORMAT_DD 0x4
+#define ISOFORMAT_DD11S 0x5
+#define AMIGAFORMAT_DD  0x6
+
