cbb6fc09063dc49a570a0a12fd9cf31cd31a8d5b
jfdelnero <jfdelnero@63fa2f70-6a9b-4bc4-b855-9c6a949b1d69>
2013-06-23 18:58:37 +0000

    Format test support added
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code@832 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- HxCFloppyEmulator/HxCFloppyEmulator_TestTools/HxCFE_StressTest/PCDOS_TestSoftware/floppyio.h
+++ HxCFloppyEmulator/HxCFloppyEmulator_TestTools/HxCFE_StressTest/PCDOS_TestSoftware/floppyio.h
@@ -1,7 +1,8 @@
 void trackseek     (unsigned char drive,unsigned char track,unsigned char head);
 void calibratedrive(unsigned char drive);
-int  write_sector (unsigned char deleted,unsigned index,unsigned char drive,unsigned char head, unsigned char track,unsigned char sector,unsigned char size,unsigned char density,unsigned char precomp,int rate);
-int  read_sector  (unsigned char deleted,unsigned index,unsigned char drive,unsigned char head, unsigned char track,unsigned char sector,unsigned char size,unsigned char density,int rate);
+int  write_sector (unsigned char deleted,unsigned index,unsigned char drive,unsigned char head, unsigned char track,unsigned char sector,unsigned int size,unsigned char density,unsigned char precomp,int rate);
+int  read_sector  (unsigned char deleted,unsigned index,unsigned char drive,unsigned char head, unsigned char track,unsigned char sector,unsigned int size,unsigned char density,int rate);
+int  format_track(int drive,unsigned char density,int track,int head,int nbsector,int sectorsize,int interleave,unsigned char formatvalue,unsigned char precomp,int rate,int gap3);
 int  chs_biosdisk (int cmd, int drive, int head, int track,int sector, int nsects, void *buf);
 void init_floppyio(void);
 void fdc_specify(unsigned char t);
