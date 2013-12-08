2d796d9f483d70aadba483c7cfde332f0f070cd0
jfdelnero <jfdelnero@63fa2f70-6a9b-4bc4-b855-9c6a949b1d69>
2013-12-08 18:53:15 +0000

    08 December 2013: HxCFloppyEmulator 2.0.18.4 / Lib 2.4.2.2
    TRS80 DMK Loaded : Image Sanity check -> Minimum track number changed from 30 to 1 track.
    
    Predefined raw formats added : Ensoniq Mirage 440KB, E-mu Emax DD 800KB, GeneralMusic GEM S3 1600KB, GeneralMusic GEM WX 1440KB.
    
    libhxcfe : Track Generator -> hxcfe_setTrackType function added.
    
    libhxcfe : Track alteration functions -> hxcfe_getSide & hxcfe_shiftTrackData function added.
    
    libhxcfe : XML floppy file definition -> <track_list></track_list> Track definition fields working.
    
    New track support : Data disk Arburg format.
    
    New track support : System disk Arburg format.
    
    New Writer : SSD Speccy DOS file.
    
    New Writer : Amiga ADF files.
    
    New Loader : SCP files (SuperCard Pro) (WIP).
    
    New Writer : SCP files (SuperCard Pro) (WIP).
    
    New Writer : Kryoflux raw files.
    
    New Writer : BMP file (Disk layout picture).
    
    libhxcfe : Transitions level flux analyzer functions.
    
    libhxcfe : Generic flux stream analyzer enhanced.
    
    ADF, DMS, MSA, ST Loaders : 4 empty tracks added at the end of the disk.
    
    D88 Loader : Odd track number issue corrected.
    
    CPC EDSK writer : Some missing flag support added. (Deleted address mark & CRC flags).
    
    Sega System24 Loader : New format added ( 8192B + 3*1024B + 512B + 256B sectors).
    
    Floppy Dump : Default step rate set at 16ms.
    
    zlib updated : zlib 1.2.8
    
    Amiga/UAE Extended ADF file loader : Amiga DOS track offset loading corrected.
    
    
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code@1053 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

---
+++ HxCFloppyEmulator/HxCFloppyEmulator_software/tags/HxCFloppyEmulator_software_V2_0_18_4/sources/gui/ff_types.h
@@ -0,0 +1,31 @@
+
+typedef struct ff_type_
+{
+	int id;
+	const char * name;
+	const char * plug_id;
+	const char * ext;
+}ff_type;
+
+enum
+{
+	FF_HFE=0,
+	FF_MFM,
+	FF_AFI,
+	FF_VTR,
+	FF_RAW,
+	FF_IMD,
+	FF_ADF,
+	FF_JV3,
+	FF_DMK,
+	FF_SDDSPECCY,
+	FF_V9T9,
+	FF_D88,
+	FF_MSA,
+	FF_HDDDA2_HFE,
+	FF_EHFE,
+	FF_ARBG,
+	FF_SKF,
+	FF_SCP,
+	FF_BMP
+};
