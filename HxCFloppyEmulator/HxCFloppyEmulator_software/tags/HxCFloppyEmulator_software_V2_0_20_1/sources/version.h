3e9b8a24c7502c9aa4adf48d01ea98bcd5e90697
jfdelnero <jfdelnero@63fa2f70-6a9b-4bc4-b855-9c6a949b1d69>
2014-01-03 16:14:54 +0000

    03 January 2014: HxCFloppyEmulator 2.0.20.1
    Disk Browser : AmigaDOS disk support added ! Amiga Files can be read/write/deleted with the Disk Browser !
    
    New Writer : XML files -> Disk layout can now be exported into XML !
    
    New Loader : XML files -> A disk can be generated from an XML file !
    
    Raw Loader : An XML file can now be used as disk layout !
    
    General : Low level sector reader and track display speed-optimized.
    
    MacOS X : Executable now compiled with the "-mmacosx-version-min=10.5" option
              -> Leopard,Snow Leopard,Lion,Mountain Lion & Mavericks supported.
    
    
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code@1083 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

---
+++ HxCFloppyEmulator/HxCFloppyEmulator_software/tags/HxCFloppyEmulator_software_V2_0_20_1/sources/version.h
@@ -0,0 +1,10 @@
+#define FILE_VERSION        2,0,20,1
+#define STR_FILE_VERSION   "2,0,20,1\0"
+#define STR_FILE_VERSION2  "2.0.20.1"
+#define STR_DATE           "01/02/2014\0"
+#define NOMFENETRE    "HxCFloppyEmulator v"STR_FILE_VERSION2
+
+#define FILEVER        FILE_VERSION
+#define PRODUCTVER     FILE_VERSION
+#define STRFILEVER     STR_FILE_VERSION
+#define STRPRODUCTVER  STR_FILE_VERSION
